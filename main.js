3e00b1ac635229cbb533446c99f5357b00f1345b
shamilmelis <akastramormoley1st@mail.ru>
2023-07-10 12:46:30 +0600

    i add all files

---
+++ main.js
@@ -0,0 +1,29 @@
+/* Задание 1 */
+
+// let a = 4;
+// let b = 55;
+//
+// console.log(a + b) >>>>>>>> Total: 59
+// console.log(a * b) >>>>>>>> Total: 220
+// console.log(a / b); >>>>>>>> Total: 0
+// console.log(a % b) >>>>>>>> Total: 4
+
+/* Задание 2 */
+
+// let a = 'Hello';
+// let b = 'Shamil';
+// let age = 16;
+// console.log(a + ' ' + b + ', ' + age) >>>>>>>> Total: Hello Shamil, 16
+
+
+/* Задание 3 */
+
+// let a = 5;
+// let b = '5';
+// console.log(a + +b) >>>>>>> Total: 10
+
+/* Задание 4 */
+
+// let c = 2;
+// let b = undefined;
+// console.log(c + b); >>>>>>>> Total: NaN
